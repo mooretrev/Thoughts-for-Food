43be752b26bb6f355dc1aeaa6f4ab65f83de15d0
priyankapaul9 <priyankapaul9@gmail.com>
2020-04-20 15:03:17 -0500

    Added Name search method

--- src/main/java/search/NameSearch.java
+++ src/main/java/search/NameSearch.java
@@ -1,6 +1,14 @@
 package search;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
 import containers.Data;
+import containers.Recipe;
 import results.Results;
 
 public class NameSearch extends AbstractSearch {
@@ -11,8 +19,15 @@ public class NameSearch extends AbstractSearch {
 
 	@Override
 	public Results search(String query) {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<Integer, Recipe> recipesByName = data.recipeById;
+		List<Recipe> finalRecipes = new Vector<>();
+
+		for(Integer id : recipesByName.keySet()){
+			if(query == recipesByName.get(id).getName()){
+				finalRecipes.add(recipesByName.get(id));
+			}
+		}
+		return new Results(finalRecipes);
 	}
 
 }
