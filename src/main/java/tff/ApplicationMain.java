1c49e23934eee49132e2cf6701b74d81083e681c
Trevor Moore <moore.trev@tamu.edu>
2020-04-20 17:38:00 -0500

    modified data class for better speed

--- src/main/java/tff/ApplicationMain.java
+++ src/main/java/tff/ApplicationMain.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 import containers.Data;
 import gui.Window;
+import results.Results;
 import search.IngredientSearch;
 import search.NameSearch;
 import search.TimeSearch;
@@ -14,7 +15,8 @@ public class ApplicationMain {
 		Data data = new Data();
 		data.load();		
 		NameSearch n = new NameSearch(data);
-		n.search("chile rellenos");
+		Results r = n.search("chile rellenos");
+		System.out.println(r.getRecipes());
 		// IngredientSearch s = new IngredientSearch(data);
 		// s.search("butterscotch chips,chinese noodles,salted peanuts");
 
