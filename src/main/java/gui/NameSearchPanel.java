f4be178cf2e82b0fde5c4d0c7e5191b3dce3e7ed
priyankapaul9 <priyankapaul9@gmail.com>
2020-04-23 15:36:06 -0500

    font

--- src/main/java/gui/NameSearchPanel.java
+++ src/main/java/gui/NameSearchPanel.java
@@ -41,14 +41,14 @@ public class NameSearchPanel extends JPanel {
         searchButton.addActionListener(new searchBtnListener(nameSearchBar));
         
         // Style gui objects
-        Font courierTitle = new Font("Courier", Font.BOLD, 27);
+        Font courierTitle = new Font("arial", Font.BOLD, 27);
         titleLabel.setPreferredSize(new Dimension(530, 530));
         titleLabel.setFont(courierTitle);
 
-        Font courierInstr = new Font("Courier", Font.PLAIN, 19);
+        Font courierInstr = new Font("arial", Font.PLAIN, 19);
         instrLabel.setFont(courierInstr);
 
-        Font courierBold = new Font("Courier", Font.BOLD, 20);
+        Font courierBold = new Font("arial", Font.BOLD, 20);
         searchButton.setFont(courierBold);
 
         // Set the Layout for the Panel
